405bf205517b148ce998273dc8fcbc273fee6aa5
alexandr- <ulianovalexandr19@gmail.com>
2021-09-23 14:17:32 +0300

    minor fix

--- routes/ftx/utils.js
+++ routes/ftx/utils.js
@@ -6,15 +6,15 @@ export const ftxQuery = async (url, {reqHeader, method, body}) => {
   const apiSecret = reqHeader['api-secret'];
   const sub = reqHeader['sub'];
 
-  const timestamp = Date.now().toString().slice(0, -3) + '000';
+  const timestamp = Date.now();
   const bodyString = isNotEmptyObject(body) ? JSON.stringify(body) : undefined;
   const payload = `${timestamp}${method.toUpperCase()}${url}${bodyString || ''}`;
   const signature = createSign(apiSecret, payload);
   const headers = {
-    'Host': 'ftx.com',
-    'Content-Type': 'application/json',
+    // 'Host': 'ftx.com',
+    // 'Content-Type': 'application/json',
     'FTX-KEY': apikey,
-    'FTX-TS': timestamp,
+    'FTX-TS': timestamp.toString(),
     'FTX-SIGN': signature,
   };
 
